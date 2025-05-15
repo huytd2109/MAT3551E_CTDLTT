cfeb7da956e6d59d25c675f6e9087be1a1cc8ff7
KumDen <quan.adn04@gmail.com>
2025-05-15 07:44:51 +0700

    Update Dictionary.java
    nigga

--- Week7/De2/Dictionary.java
+++ Week7/De2/Dictionary.java
@@ -1,47 +1,41 @@
 @SuppressWarnings("unchecked")
-public class DictList<T> implements ListInterface<T> {
+public class Dictionary {
 
-    private T[] data;
-    private int size;
-    private final int DEFAULT_CAPACITY = 100;
+    private DictList<String> viList = null; // Danh sách lưu các từ tiếng Việt
+    private DictList<String> enList = null; // Danh sách lưu các từ tiếng Anh
 
-    public DictList() {
-        data = (T[]) new Object[DEFAULT_CAPACITY];
-        size = 0;
-    }
+    // Phương thức nạp từ điển
+    public void loadDictionary(String[] vi, String[] en) {
+        viList = new DictList<String>();
+        enList = new DictList<String>();
 
-    @Override
-    public void add(T element) {
-        if (size >= data.length) {
-            resize();
+        for (int i = 0; i < vi.length; i++) {
+            viList.add(vi[i]);
+            enList.add(en[i]);
         }
-        data[size++] = element;
     }
 
-    @Override
-    public T get(int i) {
-        if (i < 0 || i >= size) {
-            throw new IndexOutOfBoundsException("Invalid index: " + i);
+    // Dịch danh sách từ tiếng Việt sang tiếng Anh
+    public DictList<String> translate(DictList<String> vi) {
+        DictList<String> result = new DictList<String>();
+
+        for (int i = 0; i < vi.size(); i++) {
+            String word = vi.get(i);
+            boolean found = false;
+
+            for (int j = 0; j < viList.size(); j++) {
+                if (viList.get(j).equals(word)) {
+                    result.add(enList.get(j));
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                result.add("UNKNOWN");
+            }
         }
-        return data[i];
-    }
 
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    // Hàm mở rộng kích thước mảng khi cần
-    private void resize() {
-        T[] newData = (T[]) new Object[data.length * 2];
-        for (int i = 0; i < data.length; i++) {
-            newData[i] = data[i];
-        }
-        data = newData;
+        return result;
     }
 }
